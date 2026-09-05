fa1ccc09380ad374de16f86b201566765a667de7
Ansh Bhavsar <anshbhavsar164@gmail.com>
2026-09-05 21:59:31 +0530

    api changes

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
+const API_URL = import.meta.env.API_URL || "http://localhost:8000";
 
 export const api = {
   addWord: async (text, source_lang, entry_type = "word") => {
